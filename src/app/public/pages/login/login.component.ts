fe15582ed4bbb2301c473f09f5f5e5058d14954f
Hector <hector.n.1192@gmail.com>
2022-08-09 23:28:06 +0200

    form login

--- src/app/public/pages/login/login.component.ts
+++ src/app/public/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,7 +8,12 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-  constructor(private router: Router) {}
+  formLogin = this.fb.nonNullable.group({
+    usuario: ['', [Validators.required, Validators.minLength(6)]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  constructor(private router: Router, private fb: FormBuilder) {}
 
   login() {
     this.router.navigate([`/private`]);
--- src/app/public/pages/login/login.module.ts
+++ src/app/public/pages/login/login.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
@@ -7,6 +8,12 @@ import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, MatInputModule, MatButtonModule, LoginRoutingModule],
+  imports: [
+    CommonModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    LoginRoutingModule,
+  ],
 })
 export class LoginModule {}
